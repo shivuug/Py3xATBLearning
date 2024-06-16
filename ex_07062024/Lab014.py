e23c6ec3a6377eb50099b8993a2ce08bb66a75ba
Python3xLearning <shivamgoyaler@gmail.com>
2024-06-16 17:20:21 +0530

    Commit till lab 13

---
+++ ex_07062024/Lab014.py
@@ -0,0 +1,24 @@
+print("Hello world")
+#commit
+#variable
+#comment
+#data types
+#int, str, float, complex, boolean
+#int
+age=45
+print(age)
+print(type(age))
+#str
+name="pramod"
+print(name)
+#float
+weight=58.6
+print(weight)
+#complex
+c=3+4j
+print(c)
+#boolean
+x=True
+print(x)
+
+print(type(x))
---
+++ ex_07062024/Lab015.py
@@ -0,0 +1,7 @@
+#advanced data types
+#list, tuple, set, dictionary, - Data set complex ones and advanced ones - API and web automation beneficial
+#take input from user
+
+user_input = input("Enter your name:")
+print("your name is", user_input)
+print(type(user_input))
